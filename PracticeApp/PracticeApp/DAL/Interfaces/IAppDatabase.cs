52f8d5ad4b3801dbcd69984e84445a56c8293357
patrickreddy <patrickreddy87@gmail.com>
2020-04-01 10:51:08 -0400

    added task methods and list methods to business logic

--- PracticeApp/PracticeApp/DAL/Interfaces/IAppDatabase.cs
+++ PracticeApp/PracticeApp/DAL/Interfaces/IAppDatabase.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PracticeApp.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +7,12 @@ namespace PracticeApp.DAL.Interfaces
 {
     interface IAppDatabase
     {
+        void RegisterUser(User newUser);
+        User GetUser(string userName);
+        void CreateList(List newList, int? id);
+        void UpdateList(List updatedList, int? id);
+        void UpdateTask(Task newTask, int? listId);
+        List GetList(int? id1, int? id2);
+        void DeleteList(List deleteList, int? id);
     }
 }
--- PracticeApp/PracticeApp/PracticeApp.cs
+++ PracticeApp/PracticeApp/PracticeApp.cs
@@ -6,16 +6,139 @@ namespace PracticeApp
 {
     public class PracticeApp
     {
+        #region Properties and Member Variables
         IAppDatabase _database;
-
-        public string CurrentUser { get; set; }
+        public User CurrentUser { get; set; }
         public Task CurrentTask { get; set; }
         public List CurrentList { get; set; }
+        #endregion
+
+        #region Constructors
         PracticeApp(IAppDatabase database)
         {
             _database = database;
         }
+        #endregion
 
+        #region User Methods
+        public void RegisterUser(User newUser)
+        {
+            User user = null;
+            try
+            {
+                user = _database.GetUser(newUser.UserName);
+            }
+            catch (Exception)
+            {
+            }
+
+            if (user != null)
+            {
+                throw new Exception("User Name is already taken. Please select a different user name");
+            }
+            else
+            {
+                _database.RegisterUser(newUser);
+                LoginUser(newUser);
+            }
+        }
+
+        public void LoginUser(User newUser)
+        {
+            User user = null;
+            try
+            {
+                user = _database.GetUser(newUser.UserName);
+            }
+            catch (Exception)
+            {
+                throw new Exception("Your login credentials did not match any of our records. Please try again.");
+            }
+
+            if (user != null)
+            {
+                CurrentUser = user;
+            }
+        }
+
+        public void LogoutUser()
+        {
+            CurrentUser = null;
+        }
+        #endregion
+
+        #region List Methods
+        public void AddNewList(List newList)
+        {
+            if(CurrentUser != null)
+            {
+                _database.CreateList(newList,CurrentUser.Id);
+            }
+            else
+            {
+                throw new Exception("You're not currently logged in. Please login again.");
+            }
+        }
+
+        public void ChangeList(List updatedList)
+        {
+            if(CurrentUser != null)
+            {
+                _database.UpdateList(updatedList, CurrentUser.Id);
+            }
+            else
+            {
+                throw new Exception("You're not currently logged in. Please login again.");
+            }
+        }
+
+        public void RemoveList(List deleteList)
+        {
+            List list = new List();
+            try
+            {
+                list = _database.GetList(deleteList.Id,CurrentUser.Id);
+            }
+            catch
+            {
+            }
+
+            if (CurrentUser != null)
+            {
+                _database.DeleteList(deleteList, CurrentUser.Id);
+            }
+            else
+            {
+                throw new Exception("You're not currently logged in. Please login again.");
+            }
+        }
+        #endregion
+
+        #region Task Methods
+        public void AddNewTask(Task newTask, int? listId)
+        {
+            if (CurrentUser != null)
+            {
+                _database.UpdateTask(newTask, listId);
+            }
+            else
+            {
+                throw new Exception("You're not currently logged in. Please login again.");
+            }
+        }
+
+        public void ChangeTask(Task updatedTask, int? listId)
+        {
+            if (CurrentUser != null)
+            {
+                _database.UpdateTask(updatedTask, listId);
+            }
+            else
+            {
+                throw new Exception("You're not currently logged in. Please login again.");
+            }
+        }
+        #endregion
 
     }
 }
